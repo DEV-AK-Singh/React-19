659689daadae24bc9b607cbaf0c1e009f79ce2ed
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-07-04 23:06:33 +0530

    implement ternary opr (single+multiple)

--- react-app-v1/src/App.jsx
+++ react-app-v1/src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   // let count = 0
@@ -12,19 +12,35 @@ function App() {
   //   console.log(count)
   // }
 
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div> 
-        <Header/>
-        <h1>Counter : {count}</h1>
+      <div>
+        {/* Component Implementation */}
+        {/* <Header/> */}
+        {/* <Footer/> */}
+
+        {/* Variable */}
+        {/* <h1>Counter : {count}</h1> */}
         {/* <button onClick={increase}>Increase using variable</button> */}
-        <button onClick={()=>setCount(count+1)}>Increase using useState</button>
-        <Footer/>
+
+        {/* useState Implementation */}
+        {/* <h1>Counter : {count}</h1>
+        <button onClick={() => setCount(count + 1)}>
+          Increase using useState
+        </button> */}
+
+        {/* Ternary Implementation */}
+        {/* <h1>
+          Counter : {count === 0 ? "Zero" : count % 5 === 0 ? "BINGO" : count}
+        </h1>
+        <button onClick={() => setCount(count + 1)}>
+          Increase
+        </button> */}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
