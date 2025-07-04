9c1d098f7a995bf20ebffa63b55df0e49151f7c1
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-07-04 20:48:44 +0530

    react 19 started

---
+++ react-app-v1/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div> 
+        <Header/>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <Footer/>
+      </div>
+    </>
+  )
+}
+
+export default App
