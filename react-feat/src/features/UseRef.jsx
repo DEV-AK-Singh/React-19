f9f5f4118fdfacfda7648e8b3998f1649188a135
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-07-06 19:08:42 +0530

    implemented useRef and Uncontrolled features

--- react-feat/src/features/UseRef.jsx
+++ react-feat/src/features/UseRef.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 export default function UseRef() {
+  const [name, setName] = useState('');
+  const nameRef = useRef(null);
+
   return(
-    <div>UseRef</div>
+    <>
+      <div>
+        <h1>Controlled Components</h1>
+        <h1>Name: {name}</h1>
+        <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)}/>
+      </div>
+      <div>
+        <h1>Uncontrolled Components</h1> 
+        <input type="text" name="nameRef" ref={nameRef}/>
+        <button onClick={() => alert(nameRef?.current.value || 'No value found')}>Update Name</button>
+      </div>
+    </>
   );
 }
